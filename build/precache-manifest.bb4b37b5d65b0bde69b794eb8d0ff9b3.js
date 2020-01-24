6c0633e6d2735212f54193e4232dceae9692718a
moli <maciejfleisz@gmail.com>
2020-01-24 02:31:12 +0100

    time to build & deploy
    build

---
+++ build/precache-manifest.bb4b37b5d65b0bde69b794eb8d0ff9b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2354fcc3835799c8a2c7",
+    "url": "/static/css/main.bf442c4f.chunk.css"
+  },
+  {
+    "revision": "2354fcc3835799c8a2c7",
+    "url": "/static/js/main.2354fcc3.chunk.js"
+  },
+  {
+    "revision": "a912238f44d94834dcbf",
+    "url": "/static/js/1.a912238f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "757cbc59bc8640ddc9c2466dce1adb17",
+    "url": "/static/media/logo.757cbc59.svg"
+  },
+  {
+    "revision": "bced1dbd66e11b281c99d2b6efc1da10",
+    "url": "/index.html"
+  }
+];
